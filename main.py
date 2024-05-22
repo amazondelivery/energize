51356aac567310e5b041752135d17966c49666f5
amazondelivery <48974775+amazondelivery@users.noreply.github.com>
2024-05-22 01:51:33 -0700

    added scenes.py and made it a lot more object oriented

--- main.py
+++ main.py
@@ -1,29 +1,56 @@
 import pygame as pg
+from scenes import *
 
-pg.init()
-titleFont = pg.font.Font("MajorMonoDisplay-Regular.ttf", 150)
-
-screen_width = 1280
-screen_height = 720
-screen = pg.display.set_mode((screen_width, screen_height))
-
-clock = pg.time.Clock()
-
-#i initially was not going to go for an object oriented designed game, but i took inspiration from
-# this video (https://youtu.be/2gABYM5M0ww?si=sUilwjJvzpKZwtn7)
+#i initially was not going to go for an object oriented designed game since the base game is rather small,
+# but i took inspiration from this video (https://youtu.be/2gABYM5M0ww?si=sUilwjJvzpKZwtn7) and i decided
+# to go with it
 class Game:
     def __init__(self):
-        print('hi')
+        pg.init()
+        pg.display.set_caption("Energize")
 
-def title():
-    text = titleFont.render("ENERGIZE", True, "White")
-    screen.blit(text, (200,200))
+        self.screen_width = 1280
+        self.screen_height = 720
+        self.screen = pg.display.set_mode((self.screen_width, self.screen_height))
+        self.clock = pg.time.Clock()
+
+        #sequences
+        self.sceneManager = SceneManager()
+
+    def tick(self):
+        self.clock.tick(60)
+
+    def mids(self, obj):
+        return (self.screen_height / 2 - obj.get_height() // 2, self.screen_width / 2 - obj.get_width() // 2)
+
+    def mid_height(self, obj):
+        return self.mids(self,obj)[0]
+
+    def mid_width(self, obj):
+        return self.mids(self,obj)[1]
+
+class SceneManager:
+    def __init__(self):
+        sequences = [TitleSequence()]
+
+        #sets intial scene to title screen
+        self.currentScene = sequences[0]
+
+    def drawScene(self):
+        self.currentScene.draw()
+
+    def getScene(self):
+        return self.currentScene.getName()
+
+
+
+game = Game()
 
 while True:
-    title()
     for event in pg.event.get():
         if event.type == pg.QUIT:
             pg.quit()
             raise SystemExit
-    clock.tick(60)
+
+    game.tick()
     pg.display.flip()
---
+++ scenes.py
@@ -0,0 +1,14 @@
+import pygame as pg
+
+class TitleSequence:
+    def __init__(self):
+        self.audioToggle = True
+        self.titleFont = pg.font.Font("MajorMonoDisplay-Regular.ttf", 150)
+
+class SettingsSequence:
+    def __init__(self):
+        print("filler")
+
+class GameScene:
+    def __init__(self):
+        print('filler')
